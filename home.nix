079cb9f6c12f45997f008ba62db96943fc236749
SirStoke <sandro.mosca.dev@gmail.com>
2022-08-18 11:28:24 +0200

    Include Bismuth-Forge/bismuth as package

--- home.nix
+++ home.nix
@@ -21,6 +21,7 @@ in
     pkgs.ungoogled-chromium
     pkgs.zoxide
     pkgs.pinentry
+    pkgs.libsForQt5.bismuth
   ];
 
   programs.neovim.enable = true;
@@ -68,5 +69,12 @@ in
 
   programs.gpg.enable = true;
 
-  services.gpg-agent.enable = true;
+  services.gpg-agent = {
+    enable = true;
+
+    # cache the keys forever so we don't get asked for a password
+    defaultCacheTtl = 31536000;
+    maxCacheTtl = 31536000;
+  };
+
 }
