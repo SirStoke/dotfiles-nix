71f885ed5b789e99c2a2ed3f24955182c160ce47
Sandro Mosca <sandro.mosca.dev@gmail.com>
2022-08-19 17:32:06 +0200

    Port homebrew module from nix-darwin

--- home-darwin.nix
+++ home-darwin.nix
@@ -2,6 +2,8 @@
   let 
     base = import ./home.nix { inherit config pkgs; };
   in recursiveUpdate base {
+    imports = [ ./modules/homebrew.nix ];
+
     programs.home-manager.enable = true;
 
     # Eventually, both sdkman and fnm will not be needed and everything will be managed by per-project nix flakes
@@ -13,4 +15,22 @@
       export PATH=/Users/sandro/.fnm:$PATH
       eval "`fnm env`"
     '';
+
+    homebrew = {
+      enable = true;
+
+      brews = [
+        "bitwarden-cli"
+        "git-machete"
+        "vault"
+        "fnm"
+        "brotli"
+      ];
+
+      casks = [
+        "amethyst"
+        "eloston-chromium"
+        "wkhtmltopdf"
+      ];
+    };
   }
