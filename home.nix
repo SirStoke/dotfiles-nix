294b2fd8e4b477a9ac71368fc359cadd933e7121
SirStoke <sandro.mosca.dev@gmail.com>
2022-11-02 11:57:16 +0100

    Use vim as the default editor

--- home.nix
+++ home.nix
@@ -60,6 +60,8 @@ in
       [ -n "$PS1" ] && \
           [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
               eval "$("$BASE16_SHELL/profile_helper.sh")"
+
+      export EDITOR=vim
     '';
   };
 
