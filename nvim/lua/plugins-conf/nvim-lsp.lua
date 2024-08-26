d53540174bf4f77138d9f32b4585c19a562c001a
SirStoke <sandro.mosca.dev@gmail.com>
2024-08-26 10:15:37 +0200

    Type info on S-M-p

--- nvim/lua/plugins-conf/nvim-lsp.lua
+++ nvim/lua/plugins-conf/nvim-lsp.lua
@@ -85,6 +85,10 @@ return {
         --  Most Language Servers support renaming across files, etc.
         map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
+        -- Rename the variable under your cursor.
+        --  Most Language Servers support renaming across files, etc.
+        map('<S-M-p>', vim.lsp.buf.hover, 'Show type')
+
         -- Execute a code action, usually your cursor needs to be on top of an error
         -- or a suggestion from your LSP for this to activate.
         map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
