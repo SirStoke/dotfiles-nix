0a274d5283dba11fd40f8c12237a3ae7d454884c
SirStoke <sandro.mosca.dev@gmail.com>
2024-09-04 21:08:27 +0000

    Minor refactor

--- daedalus/modules/duplicity.nix
+++ daedalus/modules/duplicity.nix
@@ -1,17 +1,22 @@
-{...}: {
+{pkgs, ...}: let
+  targetUrl = "s3://daedalus-bkp";
+  secretFile = /run/agenix/duplicity-secrets;
+  endpointUrl = "https://s3.eu-central-003.backblazeb2.com";
+in {
   services.duplicity = {
+    inherit targetUrl;
+    inherit secretFile;
+
     enable = true;
     root = /var/data;
-    targetUrl = "s3://daedalus-bkp";
     fullIfOlderThan = "1M";
     extraFlags = [
       "--s3-endpoint-url"
-      "https://s3.eu-central-003.backblazeb2.com"
+      endpointUrl
       "--progress"
       "--progress-rate"
       "60"
     ];
-    secretFile = /run/agenix/duplicity-secrets;
     cleanup.maxFull = 1;
   };
 }
