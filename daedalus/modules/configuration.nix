e3ba930ed1b2e290704eabc017977d52098031d9
SirStoke <sandro.mosca.dev@gmail.com>
2025-10-05 20:42:35 +0000

    Disable toggled for the moment

--- daedalus/modules/configuration.nix
+++ daedalus/modules/configuration.nix
@@ -104,6 +104,8 @@
 
   systemd.services.NetworkManager-wait-online.enable = false;
 
+  hardware.graphics.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
