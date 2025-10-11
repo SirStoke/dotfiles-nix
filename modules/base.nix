e6d8527b968b6e82400498ac41c23bdf69d4c66f
SirStoke <sandro.mosca.dev@gmail.com>
2025-10-11 21:25:15 +0200

    Enable xdg portal

--- modules/base.nix
+++ modules/base.nix
@@ -147,6 +147,10 @@ in {
 
   programs.firejail.enable = true;
 
+  boot.kernelParams = [ "amdgpu.dcdebugmask=0x10" ];
+
+  xdg.portal.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
